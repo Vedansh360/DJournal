4f365b935531941c37c318e947e79323006f4cd5
Vedansh Mishra <vedanshmishra560@gamil.com>
2023-05-09 21:39:43 +0530

    made homepage responsive

--- client/src/components/homepage/search-bar/SearchBar.jsx
+++ client/src/components/homepage/search-bar/SearchBar.jsx
@@ -3,9 +3,9 @@ import search_icon from '../resources/search_icon.png';
 export default function SearchBar() {
     
     return (
-        <form className="search-bar">
+        <div className="search-bar-container">
             <input type="text" placeholder="Search article" />
             <button type="submit"><img src={search_icon} alt="Submit" /></button>
-        </form>
+        </div>
     );
 }
