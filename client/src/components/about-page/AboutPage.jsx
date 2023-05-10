d0e70276b9743a9bad2d9f961851a0a2923ec262
Yash Goyal <yashgoyaldev@gmail.com>
2023-05-10 23:01:00 +0530

    updated about section

--- client/src/components/about-page/AboutPage.jsx
+++ client/src/components/about-page/AboutPage.jsx
@@ -9,14 +9,13 @@ export default function AboutPage() {
             <h1 style={{fontWeight: "bold"}}>About</h1>
             <br/>
             <p style={{color: "black"}}>
-                DJournal is a decentralised news sharing and journaling app. We aim to provide a platform for<br/>
-                independent journalist, news agencies and individuals to post news and other updates anonymously<br/>
-                on the blockchain without worrying about threat from any government or private organisation or<br/> 
-                their post being taken down due to disagreement or opposition from any party.<br/>
-                <br/>
-                DJournal aims to provide a solution to the problem of corrupt media and censorship through the implementation<br/>
-                of blockchain technology using smart contracts and IPFS so that no individual or organisation has control over the<br/>
-                data, so that anyone can safely but responsibly post any news or information they desire or need to be known.<br/>
+                DJournal is a revolutionary news sharing and journaling application designed to provide a platform for independent journalists, news agencies, and individuals to post news and other updates anonymously on the blockchain. Our primary goal is to enable free and unbiased sharing of information without any fear of censorship or retribution from any government or private organization.
+            </p>
+            <p style={{color: "black"}}>
+                The current media landscape is plagued by corruption, censorship, and bias. DJournal aims to address these issues by leveraging the power of blockchain technology and implementing smart contracts and IPFS. With this approach, we can ensure that no individual or organization has control over the data, and anyone can safely post any news or information they desire without worrying about it being taken down or manipulated.
+            </p>
+            <p style={{color: "black"}}>
+                We believe in the responsible sharing of information, and our platform is designed to enable this in a decentralized and secure manner. We provide a solution to the problem of corrupt media and censorship by creating an open and transparent environment for the exchange of ideas and information. We believe that this approach will lead to a more informed and empowered society that is better equipped to make important decisions.
             </p>
         </div>
     );
